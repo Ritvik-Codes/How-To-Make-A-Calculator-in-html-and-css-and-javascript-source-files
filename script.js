cd9b362ea8f11754f266e527167f82e587be4793
Ritvik Codes <88265166+Ritvik-Codes@users.noreply.github.com>
2021-08-01 12:12:59 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,19 @@
+function add() {
+    var num = Number(document.getElementById("number1").value) + Number(document.getElementById("number2").value);
+    window.alert(num);
+}
+
+function sub() {
+    var num = Number(document.getElementById("number1").value) - Number(document.getElementById("number2").value);
+    window.alert(num);
+}
+
+function mul() {
+    var num = Number(document.getElementById("number1").value) * Number(document.getElementById("number2").value);
+    window.alert(num);
+}
+
+function div() {
+    var num = Number(document.getElementById("number1").value) / Number(document.getElementById("number2").value);
+    window.alert(num);
+}
